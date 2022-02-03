73df7ffaaef37007ddc976be82d1f944133c7aaa
Shahab <Shahab@SHAHAB>
2022-02-03 21:28:30 +0330

    Add project files.

---
+++ src/Web/HealthChecks/ApiHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using BlazorShared;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Microsoft.eShopWeb.Web.HealthChecks;
+
+public class ApiHealthCheck : IHealthCheck
+{
+    private readonly BaseUrlConfiguration _baseUrlConfiguration;
+
+    public ApiHealthCheck(BaseUrlConfiguration baseUrlConfiguration)
+    {
+        _baseUrlConfiguration = baseUrlConfiguration;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default(CancellationToken))
+    {
+        string myUrl = _baseUrlConfiguration.ApiBase + "catalog-items";
+        var client = new HttpClient();
+        var response = await client.GetAsync(myUrl);
+        var pageContents = await response.Content.ReadAsStringAsync();
+        if (pageContents.Contains(".NET Bot Black Sweatshirt"))
+        {
+            return HealthCheckResult.Healthy("The check indicates a healthy result.");
+        }
+
+        return HealthCheckResult.Unhealthy("The check indicates an unhealthy result.");
+    }
+}
